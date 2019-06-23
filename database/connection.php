c6493b7c567ff3e8a03f2169a2033bcd9e83771c
Deyan Dimov <41387847+deyandim@users.noreply.github.com>
2019-06-23 14:28:07 +0300

    Add files via upload

---
+++ database/connection.php
@@ -0,0 +1,2 @@
+<?php
+$db = new PDO("mysql:host=localhost;dbname=forum", "root", "", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
---
+++ templates/categories_form.php
@@ -0,0 +1,11 @@
+
+
+
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Question</th>
+        <th>Answer</th>
+    </tr>
+</table>
